a32ca51dde815ef17936925219169515012fbc51
Yuri Victor <yurivictor@gmail.com>
2016-12-30 13:28:30 -0500

    Refactors for extensability

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
+// Require the library
 var are_we_off_today = require( './index.js' );
 
 // Stop function if we are not working today
 if ( are_we_off_today() ) { return; }
 
+// Go on with whatever you want to do
 console.log( 'We are working today.' );
